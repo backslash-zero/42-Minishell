3ec17ba60a59bf489d5bd37fe87a8e6a1804af79
rzafari42 <redwane.zafari@hotmail.com>
2020-05-26 19:31:43 +0200

    prompt

---
+++ minishell.h
@@ -0,0 +1,11 @@
+#ifndef _MINISHELL_H
+# define _MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <string.h>
+# include <errno.h>
+# include "libft/libft.h"
+
+#endif
