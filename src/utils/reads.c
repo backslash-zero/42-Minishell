0f242f8d8bf78a16c0fc0180b7e096f1c5affc3f
rzafari42 <redwane.zafari@hotmail.com>
2020-05-28 00:58:09 +0200

    builtsin info

--- src/utils/reads.c
+++ src/utils/reads.c
@@ -1,6 +1,90 @@
 #include "../../incs/minishell.h"
 
-void	launch(char *input)
+void	ft_builtinstab(t_parse *parse)
+{
+	parse->builtnb[0] = "echo";
+	parse->builtnb[1] = "cd";
+	parse->builtnb[2] = "pwd";
+	parse->builtnb[3] = "export";
+	parse->builtnb[4] = "unset";
+	parse->builtnb[5] = "env";
+	parse->builtnb[6] = "exit";
+}
+
+int		ft_echo(void)
+{
+	printf("i'm in echo\n");
+	return(1);
+}
+
+int		ft_cd(void)
+{
+	printf("i'm in cd\n");
+	return(1);
+}
+
+int		ft_pwd(void)
+{
+	printf("i'm in pwd\n");
+	return(1);
+}
+
+int		ft_export(void)
+{
+	printf("i'm in export\n");
+	return(1);
+}
+
+int		ft_unset(void)
+{
+	printf("i'm in unset\n");
+	return(1);
+}
+
+int		ft_env(void)
+{
+	printf("i'm in env\n");
+	return(1);
+}
+
+int		ft_exit(void)
+{
+	printf("i'm in exit\n");
+	return(1);
+}
+
+builtfunc_addr	builtins_func[] = {
+	&ft_echo, &ft_cd, &ft_pwd, &ft_export, &ft_unset, &ft_env, &ft_exit
+};
+
+int		ft_checkbuiltins(char *s, t_parse *parse)
+{
+	int i;
+	int	good;
+
+	i = 0;
+	good = 0;
+	while (i <= NB_BUILINS && good != 1)
+	{
+		if (ft_strcmp(s, parse->builtnb[i]) == 0)
+			good = 1;
+		if (good != 1)
+			i++;
+	}
+	if (good == 1)
+	{
+		builtins_func[i]();
+		return(1);
+	}
+	else
+		return (0);
+	/*A amelioer en faisant un exit avec un message d'erreur*/
+}
+
+
+
+
+void	launch(char *input, t_parse *parse)
 {
     char    *s;
     char    *p;
@@ -18,9 +102,11 @@ void	launch(char *input)
 	}
 	if (process == 0)
 	{
-		s = ft_strjoin("/bin/", arg_list[0]);
+		if (ft_checkbuiltins(arg_list[0], parse) == 0)
+			exit(0);
+		/*s = ft_strjoin("/bin/", arg_list[0]);
 		if ((execve(s, arg_list, NULL)) == -1)
-			ft_putstr(strerror(errno));
+			ft_putstr(strerror(errno));*/
 	}
 	else
 		wait(&process);
@@ -31,7 +117,10 @@ void	prompt(void)
 {
 	char	buffer[MAX_INPUT_SIZE];
 	int		ret;
-	
+	t_parse	parse;
+	int i = 0;
+
+	ft_builtinstab(&parse);
 	while (1)
 	{
 		print_prompt_prefix();
@@ -42,6 +131,6 @@ void	prompt(void)
 			buffer[ret - 1] = '\0';
 		else
 			ft_strlcpy(buffer, "exit", 5);
-		launch(buffer);
+		launch(buffer, &parse);
 	}
 }
