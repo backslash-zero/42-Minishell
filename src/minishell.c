633548f7147e50879bfcfd538249991a0f58bd13
rzafari42 <redwane.zafari@hotmail.com>
2020-06-12 22:08:36 +0200

    env variable added to export

--- src/minishell.c
+++ src/minishell.c
@@ -7,6 +7,7 @@ int		main(int ac, char **av, char **envp)
 	(void)ac;
 	(void)av;
 	g_env = init_env(&g_env, envp);
+	g_export = init_env(&g_export, envp);
 	prompt();
 	ft_lstclear(&g_env, free);
 	ft_lstclear(&g_export, free);
