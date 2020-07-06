3808844060cccf9e031dc47988eef2c1b52d82c1
rzafari42 <redwane.zafari@hotmail.com>
2020-07-07 00:18:57 +0200

    echo adapted

--- incs/builtin.h
+++ incs/builtin.h
@@ -1,6 +1,7 @@
 #ifndef BUILTIN_H
 # define BUILTIN_H
 
+void		echo_print(char **arg, int i, int fd);
 int     builtin_echo(int fd, char **arg);
 int		builtin_cd(int fd, char **arg);
 int		builtin_pwd(int fd, char **arg);
