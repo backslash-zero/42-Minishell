b4c84ad94b50454a21e60cb4631ffc88393e72b8
Redwane ZAFARI <rzafari@e1r12p8.42.fr>
2020-09-21 14:17:58 +0200

    echo -nnnn fixed

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/19 10:59:32 by rzafari           #+#    #+#             */
-/*   Updated: 2020/09/21 11:33:19 by rzafari          ###   ########.fr       */
+/*   Updated: 2020/09/21 14:16:38 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,35 @@ void	echo_print(char **arg, int i)
 
 int		builtin_echo(char **arg)
 {
+	int i;
+	int j;
+
 	if (arg_len(arg) == 1)
 	{
 		ft_putchar('\n');
 		g_ret = 0;
 		return (1);
 	}
-	if (ft_strcmp(arg[1], "-n") == 0)
-		echo_print(arg, 2);
+	i = 1;
+	if (arg[i][0] == '-')
+	{
+		while (arg[i])
+		{
+			j = 0;
+			while (arg[i][j])
+			{
+				if (arg[i][j] == '-' || arg[i][j] == 'n')
+					j++;
+				else
+				{
+					echo_print(arg, i);
+					g_ret = 0;
+					return (0);
+				}
+			}
+			i++;
+		}
+	}
 	else
 	{
 		echo_print(arg, 1);
